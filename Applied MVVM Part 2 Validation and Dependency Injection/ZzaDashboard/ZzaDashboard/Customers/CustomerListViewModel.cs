27469f7a1cbea88274e1a6527ebea4844d1addd7
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-06-21 23:02:14 +0400

    Applied MVVM Parts

---
+++ Applied MVVM Part 2 Validation and Dependency Injection/ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Zza.Data;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard.Customers
+{
+    public class CustomerListViewModel:BindableBase
+    {
+        private ICustomersRepository _repo;
+
+        private ObservableCollection<Customer> _customers;
+
+        private List<Customer> _allCustomers;
+        private string _searchInput;
+
+
+        public CustomerListViewModel(ICustomersRepository repo)
+        {
+            _repo = repo;
+            PlaceOrderCommand = new RelayCommand<Customer>(OnPlaceOrder);
+            AddCustomerCommand = new RelayCommand(OnAddCustomer);
+            EditCustomerCommand = new RelayCommand<Customer>(OnEditCustomer);
+            ClearSearchCommand = new RelayCommand(OnClearSearch);
+        }
+
+        private void OnClearSearch()
+        {
+            SearchInput = null;
+        }
+
+        public ObservableCollection<Customer> Customers
+        {
+            get => _customers;
+            set => base.SetProperty(ref _customers, value);
+        }
+
+        public string SearchInput
+        {
+            get => _searchInput;
+            set
+            {
+                base.SetProperty(ref _searchInput, value);
+
+                FilterCustomers(_searchInput);
+            }
+        }
+
+        private void FilterCustomers(string searchInput)
+        {
+            if (string.IsNullOrWhiteSpace(searchInput))
+            {
+                Customers = new ObservableCollection<Customer>(_allCustomers);
+            }
+            else
+            {
+                Customers = new ObservableCollection<Customer>(_allCustomers.Where(c =>
+                    c.FullName.ToLower().Contains(searchInput.ToLower())));
+
+            }
+        }
+
+        public ICommand PlaceOrderCommand { get; private set; }
+        public ICommand AddCustomerCommand { get; set; }
+        public ICommand EditCustomerCommand { get; set; }
+
+        public ICommand ClearSearchCommand { get; private set; }
+
+        public event Action<Guid> PlaceOrderRequested;
+        public event Action<Customer> AddCustomerRequested;
+        public event Action<Customer> EditCustomerRequested;
+
+        public async void LoadCustomers()
+        {
+            _allCustomers = await _repo.GetCustomersAsync();
+
+            Customers = new ObservableCollection<Customer>(_allCustomers);
+        }
+
+        private void OnPlaceOrder(Customer customer)
+        {
+            PlaceOrderRequested?.Invoke(customer.Id);
+        }
+
+
+        private void OnEditCustomer(Customer customer)
+        {
+            EditCustomerRequested?.Invoke(customer);
+        }
+
+        private void OnAddCustomer()
+        {
+            AddCustomerRequested?.Invoke(new Customer()
+            {
+                Id = Guid.NewGuid(),
+            });
+        }
+
+    }
+}
---
+++ Applied MVVM Part 2 Validation and Dependency Injection/ZzaDashboard/ZzaDashboard/MainWindowViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Windows.Input;
+using Unity;
+using Zza.Data;
+using ZzaDashboard.Customers;
+using ZzaDashboard.OrderPrep;
+using ZzaDashboard.Orders;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard
+{
+    public class MainWindowViewModel:BindableBase
+    {
+        private CustomerListViewModel _customerListViewModel;
+        private OrderViewModel _orderViewModel;
+        private OrderPrepViewModel _orderPrepViewModel;
+        private AddEditCustomerViewModel _addEditCustomerViewModel;
+        private BindableBase _currentViewModel;
+        public MainWindowViewModel()
+        {
+            _customerListViewModel = ContainerHelper.Container.Resolve<CustomerListViewModel>();
+            _orderViewModel = new OrderViewModel();
+            _orderPrepViewModel = new OrderPrepViewModel();
+            _addEditCustomerViewModel = ContainerHelper.Container.Resolve<AddEditCustomerViewModel>();
+            _addEditCustomerViewModel.Done += NavToCustomerList;
+
+            _customerListViewModel.PlaceOrderRequested += NavToOrder;
+            _customerListViewModel.AddCustomerRequested += NavToAddCustomer;
+            _customerListViewModel.EditCustomerRequested += NavToEditCustomer;
+
+            NavCommand = new RelayCommand<string>(OnNav);
+        }
+
+        private void NavToCustomerList()
+        {
+            CurrentViewModel = _customerListViewModel;
+        }
+
+        public ICommand NavCommand { get; private set; }
+
+        public BindableBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => base.SetProperty(ref _currentViewModel, value);
+        }
+
+        private void OnNav(string navigation)
+        {
+            switch (navigation)
+            {
+                case "order":
+                {
+                    CurrentViewModel = _orderPrepViewModel;
+                }
+                    break;
+                case "orderPrep":
+                {
+                    CurrentViewModel = _orderViewModel;
+                }
+                    break;
+                default:
+                {
+                    CurrentViewModel = _customerListViewModel;
+                }
+                    break;
+            }
+        }
+
+        private void NavToOrder(Guid customerId)
+        {
+            _orderViewModel.CustomerId = customerId;
+
+            CurrentViewModel = _orderViewModel;
+        }
+
+
+        private void NavToEditCustomer(Customer customer)
+        {
+            _addEditCustomerViewModel.EditMode = true;
+            _addEditCustomerViewModel.SetCustomer(customer);
+
+            CurrentViewModel = _addEditCustomerViewModel;
+        }
+
+        private void NavToAddCustomer(Customer customer)
+        {
+            _addEditCustomerViewModel.EditMode = false;
+            _addEditCustomerViewModel.SetCustomer(customer);
+
+            CurrentViewModel = _addEditCustomerViewModel;
+        }
+
+    }
+}
